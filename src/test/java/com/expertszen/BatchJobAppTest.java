0cda773dd492ca7838f18e7d39bf8e35cebb7b26
sukanyasuresh-1211 <sukanyasuresh1211@gmail.com>
2025-10-30 17:27:02 +0530

    Add failing test case for error validation
    Added a failing test case to validate error handling.

--- src/test/java/com/expertszen/BatchJobAppTest.java
+++ src/test/java/com/expertszen/BatchJobAppTest.java
@@ -2,10 +2,15 @@ package com.expertszen;
 
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class BatchJobAppTest {
     @Test
     public void testApp() {
         assertTrue(true);
+    }
+    @Test
+    public void testShouldFail() {
+        assertEquals(1, 2); //This will always fail
     }
 }
